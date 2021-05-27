b6100bc6f7c62eb2912424d1d9dc821ee5429b7a
ryan <ball.ryan.rb@gmail.com>
2021-05-27 17:02:42 -0600

    stores API key in credentials file

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < ApplicationController
   def index
-    response = HTTP.headers("Content-Type" => "application/json", "Authorization" => "Token token='ab538857f72b32b50f31debb86d2f269'").get("https://favqs.com/api/qotd")
+    response = HTTP.headers("Content-Type" => "application/json", "Authorization" => "Token token='Rails.application.credentials.secured_api_key!'").get("https://favqs.com/api/qotd")
     render json: response.parse(:json)
   end
   # GET requests from the Fav Quotes API require two headers:
