3e73456583b928cbdfb3b151337f68d849360d77
ryan <ball.ryan.rb@gmail.com>
2021-05-27 16:45:56 -0600

    adds QOTD get request

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,9 @@
 class QuotesController < ApplicationController
+  def index
+    response = HTTP.headers("Content-Type" => "application/json", "Authorization" => "Token token='ab538857f72b32b50f31debb86d2f269'").get("https://favqs.com/api/qotd")
+    render json: response.parse(:json)
+  end
+  # GET requests from the Fav Quotes API require two headers:
+  # Content-Type: application/json
+  # Authorization: Token token="API-KEY"
 end
